f0270df98512af3b5a907cd2da51f2764bc6c6e8
OlgaDonchenko <n0931606045@gmail.com>
2024-03-26 23:15:22 +0200

    dfdfdffdf

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,14 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-
-function App() {
-  const [count, setCount] = useState(0);
-
+export default function App() {
+  const o = "i love d";
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + Reactgoit</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+    <dev>
+      <h1>{o}</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore aliquid
+        sed aliquam voluptates laboriosam ea voluptatem consectetur minima
+        pariatur iste distinctio, omnis reprehenderit nam quae. Modi autem totam
+        sequi dolore.
       </p>
-    </>
+    </dev>
   );
 }
-
-export default App;

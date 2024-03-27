4c3935acba77a34d06a0b2ab79407086a38ac3a1
OlgaDonchenko <n0931606045@gmail.com>
2024-03-27 12:30:18 +0200

    ісісі

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 export default function App() {
   const o = "i love d";
+  const imageURL =
+    "https://i.natgeofe.com/n/548467d8-c5f1-4551-9f58-6817a8d2c45e/NationalGeographic_2572187_square.jpg";
+
   return (
     <dev>
       <h1>{o}</h1>
+      <img src={imageURL} alt="" width="480" />
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore aliquid
         sed aliquam voluptates laboriosam ea voluptatem consectetur minima
